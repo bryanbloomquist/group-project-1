44d83fdd8eb08c318878e397f17815870dcac65f
bloomy215 <bmbloomquist@gmail.com>
2019-01-28 21:26:14 -0600

    pull monster from dnd array code written

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -12,6 +12,9 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
+var userMonsterHP = "";
+var userMonsterAC = "";
+var userMonsterName= "";
 
 // When Add Character button is clicked
 $("#new-character").on("click", function (event) {
@@ -26,9 +29,36 @@ $("#load-character").on("click", function (event) {
 })
 
 // When Load Monster button is clicked
-$("#load-monster").on("click", function (event) {
-
-
+$("#load-monster").on("click", function(event) {
+    event.preventDefault();
+    var monster=$("#user-monster").val().trim();
+    var upperCaseMonster = "";
+    var monsterWords = monster.split(" ");
+    for(var i = 0; i<monsterWords.length;i++) {
+        var monster = monsterWords[i];
+        monster = monster.charAt(0).toUpperCase()+monster.slice(1);
+        upperCaseMonster = upperCaseMonster + monster + " ";
+    }
+    upperCaseMonster.trim()
+    var queryURL = "http://www.dnd5eapi.co/api/monsters/?name="+upperCaseMonster;
+    $.ajax({
+        url: queryURL, method: "GET"
+    }).then(function(response){
+        var results = response.results[0].url;
+        $.ajax({
+            url: results, method: "GET"
+        }).then(function(response){
+            userMonsterHP = response.hit_points;
+            userMonsterAC = response.armor_class;
+            userMonsterName = response.name;
+            database.ref().push({
+                maxHealth: userMonsterHP,
+                currentHP: userMonsterHP,
+                ArmorClass: userMonsterAC,
+                name: userMonsterName
+            })
+        })
+    })
 })
 
 // When Advance Initiative button is clicked
